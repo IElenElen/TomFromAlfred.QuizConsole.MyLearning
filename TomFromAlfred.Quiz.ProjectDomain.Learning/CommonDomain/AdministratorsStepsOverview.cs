aacebb97e30ca3ea40343caae713a77064a0cfc9
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-05-05 14:58:43 +0200

    16.02 Division into main quiz, project app and domain.

---
+++ TomFromAlfred.Quiz.ProjectDomain.Learning/CommonDomain/AdministratorsStepsOverview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomFromAlfred.Quiz.ProjectDomain.Learning.CommonDomain
+{
+    public class AdministratorsStepsOverview
+    {
+        public int CreatedById { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public int? ModifiedById { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+    }
+}
