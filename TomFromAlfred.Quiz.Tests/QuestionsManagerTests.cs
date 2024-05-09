bf45f00190854eb2068fc255ae26f160333ec9e8
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-05-09 15:04:54 +0200

    09.05.24: 1 approach to the test

---
+++ TomFromAlfred.Quiz.Tests/QuestionsManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomFromAlfred.Quiz.ProjectApp.Learning.ManagerApp;
+using TomFromAlfred.Quiz.ProjectApp.Learning.ServiceApp;
+using TomFromAlfred.Quiz.ProjectDomain.Learning.Entity;
+
+namespace TomFromAlfred.Quiz.Tests
+{
+    public class QuestionsManagerTests
+    {
+        [Fact]
+        public void Constructor_AddsAllQuestionsFromQuestionService()
+        {
+            // Arrange
+            var mockQuestionService = new Mock<QuestionServiceApp>(); // Tworzenie fałszywego obiektu QuestionServiceApp
+            var fakeQuestions = new List<Question>
+                {
+            new Question { QuestionNumber = 9, QuestionContent = "xyz"}
+            //new Question { QuestionNumber = 11, QuestionContent = "Example question 11" },
+        };
+            mockQuestionService.Setup(x => x.AllQuestions).Returns(fakeQuestions); // Konfiguracja zachowania fałszywego obiektu
+
+            // Act
+            var managerApp = new QuestionsManagerApp();
+            var actualQuestions = managerApp.Questions;
+
+            // Assert
+            Assert.Equal(fakeQuestions.Count, actualQuestions.Count); // Sprawdzenie, czy liczba pytań zgadza się
+                                                                      
+            // Sprawdź, czy wszystkie pytania z QuestionServiceApp zostały dodane do listy Questions
+            foreach (var expectedQuestion in fakeQuestions)
+            {
+                Assert.Contains(expectedQuestion, actualQuestions);
+            }
+        }
+    }
+}
---
+++ TomFromAlfred.Quiz.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TomFromAlfred.Quiz.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
