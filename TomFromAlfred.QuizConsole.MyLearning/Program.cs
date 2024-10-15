f372308155677bb5f483e0fc3328f77f35e9b384
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-10-15 17:29:27 +0200

    New approach to the project. The vision of the quiz in the comment: Program class.

--- TomFromAlfred.QuizConsole.MyLearning/Program.cs
+++ TomFromAlfred.QuizConsole.MyLearning/Program.cs
@@ -1,6 +1,35 @@
 ﻿
 namespace TomFromAlfred.QuizConsole.MyLearning
 {
+    /*
+     Projekt: Quiz.
+
+     Funkcjonalności:
+
+     Wyświetlenie zestawów quizu, po kolei. Najpierw nr 1, czekanie na odpowiedź, weryfikacja odpowiedzi, potem zestaw nr 2 itd.
+
+     Numeruję zestaw jako całość, który wyświetla się użytkwnikowi. Czy to dobry pomysł???
+
+     Odliczanie czasu??? Może się nie udać...
+
+     Wynik: zliczanie poprawnych odpowiedzi + podanie procentowe poprawności. Wynik na koniec quizu.
+
+     Losowanie zestawów?
+
+     Wyjście z quiz w każdym momencie.
+
+     Budowa:
+
+     Interfejs jaki?
+     
+     Klasa wspólna: dla crud klasa serwisowa, dla pracy na plikach json - też klasa serwisowa
+     
+     Menadżery
+
+     Serwisy: 
+
+     Testy w xunit: jednostkowe i integracyjne
+     */
 
     public class Program //zmiana widoczności kolejnych klas
     {
